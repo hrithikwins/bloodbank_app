9654a72e55a853969c9f58379f580ec050f8679e
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2023-01-17 16:26:30 +0530

    created splash screen

--- lib/screens/hello.dart
+++ lib/screens/hello.dart
@@ -1,11 +1,18 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Image.asset(
+            "assets/bloodbank.png",
+          ),
+        ),
+      ),
+    );
   }
 }
