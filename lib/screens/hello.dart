fe0305f233a313809a31e20f7d676f794fac66d4
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2023-01-17 16:53:36 +0530

    local commit

--- lib/screens/hello.dart
+++ lib/screens/hello.dart
@@ -7,12 +7,13 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Image.asset(
-            "assets/bloodbank.png",
-          ),
-        ),
-      ),
+          child: Column(
+        children: [
+          Container(
+            child: ,
+          )
+        ],
+      )),
     );
   }
 }
