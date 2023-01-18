aa770ca97b119d7a994fd115f207397039b9094d
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2023-01-18 16:31:33 +0530

    removed background color

--- lib/screens/stack-assignment.dart
+++ lib/screens/stack-assignment.dart
@@ -9,70 +9,67 @@ class MyHomePage extends StatelessWidget {
       body: SafeArea(
         child: SizedBox(
           width: MediaQuery.of(context).size.width,
-          child: Container(
-            color: Colors.grey.shade100,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.all(8),
-                  child: Text(
-                    "Login with your mobile",
-                  ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.all(8),
+                child: Text(
+                  "Login with your mobile",
                 ),
-                Stack(
-                  clipBehavior: Clip.none,
-                  alignment: Alignment.center,
-                  // fit: StackFit.loose,
-                  // mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(
-                        left: 6,
-                        top: 6,
-                        right: 22,
-                        bottom: 6,
+              ),
+              Stack(
+                clipBehavior: Clip.none,
+                alignment: Alignment.center,
+                // fit: StackFit.loose,
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(
+                      left: 6,
+                      top: 6,
+                      right: 22,
+                      bottom: 6,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 0.8,
+                        color: Colors.blueGrey,
                       ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(
+                          10,
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      "94999999",
+                    ),
+                  ),
+                  Positioned(
+                    right: -10,
+                    child: Container(
                       decoration: BoxDecoration(
+                        color: Colors.white,
                         border: Border.all(
                           width: 0.8,
                           color: Colors.blueGrey,
                         ),
                         borderRadius: BorderRadius.all(
                           Radius.circular(
-                            10,
+                            8,
                           ),
                         ),
                       ),
-                      child: Text(
-                        "94999999",
-                      ),
-                    ),
-                    Positioned(
-                      right: -10,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(
-                            width: 0.8,
-                            color: Colors.blueGrey,
-                          ),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(
-                              8,
-                            ),
-                          ),
-                        ),
-                        child: Icon(
-                          Icons.check,
-                        ),
+                      child: Icon(
+                        Icons.check,
                       ),
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
