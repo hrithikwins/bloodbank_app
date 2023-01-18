5d78953682f7ef4761090e0bccfc2acc6753016e
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2023-01-18 19:45:09 +0530

    created phone number scren

--- lib/screens/hello.dart
+++ lib/screens/hello.dart
@@ -7,7 +7,37 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(),
+        child: Column(
+          children: [
+            //the enter number field
+            Container(
+              child: Column(
+                children: [
+                  Container(
+                    child: Text(
+                      "Enter your mobile number",
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(60, 14, 60, 17),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 0.8,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            20,
+                          ),
+                        )),
+                    child: Text(
+                      "Enter your mobile number",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
