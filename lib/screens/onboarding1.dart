e4744c8ee70d95ee1af24fbf71b78a5e62a46675
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2023-01-18 17:00:40 +0530

    onboarding screen from data

--- lib/screens/onboarding1.dart
+++ lib/screens/onboarding1.dart
@@ -1,3 +1,4 @@
+import 'package:bloodbank_app/constants/onboarding_data.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -6,35 +7,39 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text("Hello"),
-      // ),
       body: SafeArea(
         child: Container(
           color: Colors.red,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                child: Image.asset(
-                  "assets/health-check.png",
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(
-                  left: 57.0,
-                  right: 56.0,
-                ),
-                child: Text(
-                  "To be a responsible donor, you must get a check-up",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: onboardingData
+                .map(
+                  (e) => Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        child: Image.asset(
+                          "assets/health-check.png",
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(
+                          left: 57.0,
+                          right: 56.0,
+                        ),
+                        child: Text(
+                          "To be a responsible donor, you must get a check-up",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-                ),
-              )
-            ],
+                )
+                .toList(),
           ),
         ),
       ),
