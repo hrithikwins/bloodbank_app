3b511fcf417dc429a37e6d677e9e370a637d4b3b
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2023-01-19 15:49:38 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bllood Bank App',
+      title: 'Blood Bank App',
       home: const MyHomePage(),
     );
   }
