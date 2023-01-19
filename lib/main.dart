eaca9eb7236d7feeac6227f34fdff395f033a12c
Sampad Adhikary <sampadadhikary031@gmail.com>
2023-01-19 19:23:19 +0530

    added title

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: 'Blood Bank App',
       home: SplashScreen(),
       //minor
     );
