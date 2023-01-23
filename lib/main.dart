44c03591375f871a446f2749f92dbc13a58f11e9
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2023-01-23 16:00:34 +0530

    pushing the navigation code

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyOnboardingPage(),
+      home: MySplashScreen(),
     );
   }
 }
--- lib/screens/onboarding/onboarding1.dart
+++ lib/screens/onboarding/onboarding1.dart
@@ -10,12 +10,17 @@ class MyOnboardingPage1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          color: Colors.red,
-          width: double.infinity,
-          child: onboardingWidget(
-            imageUrl: onboardingData[0]["image_url"]!,
-            text: onboardingData[0]["text"]!,
+        child: InkWell(
+          onTap: () => {
+            Navigator.pop(context),
+          },
+          child: Container(
+            color: Colors.red,
+            width: double.infinity,
+            child: onboardingWidget(
+              imageUrl: onboardingData[1]["image_url"]!,
+              text: onboardingData[1]["text"]!,
+            ),
           ),
         ),
       ),
