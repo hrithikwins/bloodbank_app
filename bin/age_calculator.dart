3b52b799a6ce226d476165acddef4e035cd21447
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2023-01-24 15:21:32 +0530

    gitignore fixed

---
+++ bin/age_calculator.dart
@@ -0,0 +1,19 @@
+void main() {
+  // var now = DateTime.now();
+  // var jan = DateTime(2000);
+  // DateTime jan = DateTime(2002, 1, 24);
+  // DateTime janUtc = DateTime.utc(2002, 1, 24);
+  // DateTime someDate = DateTime.parse('2002-01-24 00:00:00.000Z');
+  // calculate date of birth
+  calculateAge(1990, 3, 3);
+  // print(someDate);
+}
+
+void calculateAge(int year, int month, int day) {
+  DateTime startDate = DateTime(year, month, day);
+  DateTime now = DateTime.now();
+
+  var difference = now.difference(startDate);
+  print(difference);
+  // print(difference.inDays / 365);
+}
