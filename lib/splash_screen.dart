f1d24f44bfec5f92e754a48c5b474ce61af533f4
Shivam0118 <shivam8728949475@gmail.com>
2023-01-24 20:53:11 +0530

    color changed and button inserted

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/material/elevated_button.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
@@ -6,6 +7,14 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.red.shade50,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {print("Im Pressed")},
+        backgroundColor: Colors.red,
+        child: Icon(
+          Icons.add,
+        ),
+      ),
       body: SafeArea(
         child: Center(
           child: Image.asset(
