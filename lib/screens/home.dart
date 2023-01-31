4800abd7e9e686d5710a290cb0af6e83d1d57c5e
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2023-01-31 17:12:47 +0530

    home screen layouting done

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,28 +5,62 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
-        key: _scaffoldKey,
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              Container(
-                child: Text("Tiles"),
-              ),
-            ],
-          ),
-        ),
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () => {
-              _scaffoldKey.currentState!.openDrawer(),
-            },
-            icon: Icon(
-              Icons.menu,
+      key: _scaffoldKey,
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            Container(
+              child: Text("Tiles"),
             ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => {
+            _scaffoldKey.currentState!.openDrawer(),
+          },
+          icon: Icon(
+            Icons.menu,
           ),
         ),
-        body: Container());
+      ),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: homeInfoBoxes(),
+        ),
+      ),
+    );
+  }
+
+  Column homeInfoBoxes() {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(29.0),
+          child: Text("hello Hrithik"),
+        ),
+        // the row with two columns
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [bloodBankInfoBox(), bloodBankInfoStatus()],
+        )
+      ],
+    );
+  }
+
+  Column bloodBankInfoBox() {
+    return Column(
+      children: [Text("Blood Group"), Text("Blood Goup ")],
+    );
+  }
+
+  Column bloodBankInfoStatus() {
+    return Column(
+      children: [Text("Blood Group"), Text("Blood Goup ")],
+    );
   }
 }
