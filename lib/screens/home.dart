8d37444c64f3557e539f329a351b150de79e79a1
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2023-01-31 19:19:19 +0530

    home page layouting

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:bloodbank_app/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -7,34 +8,91 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
-        key: _scaffoldKey,
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              Container(
-                child: Text("Some text here"),
+      key: _scaffoldKey,
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            Container(
+              child: Text("Some text here"),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              // Scaffold.of(context).openDrawer();
+              _scaffoldKey.currentState!.openDrawer();
+            },
+            icon: Icon(Icons.menu)),
+      ),
+      body: Container(
+        color: MyColors.redPrimary,
+        width: double.infinity,
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(29.0),
+              child: Text(
+                "Hello Hrithik",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
               ),
-            ],
-          ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                bloodGroupInfoWidget(context),
+                bloodDonationInfoWidget(context),
+              ],
+            )
+          ],
         ),
-        appBar: AppBar(
-          leading: IconButton(
-              onPressed: () {
-                // Scaffold.of(context).openDrawer();
-                _scaffoldKey.currentState!.openDrawer();
-              },
-              icon: Icon(Icons.menu)),
-        ),
-        body: SizedBox(
-          width: double.infinity,
+      ),
+    );
+  }
+
+  Widget bloodDonationInfoWidget(context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.41,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
           child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(29.0),
-                child: Text("Hello Shreyash"),
+            children: const [
+              Text(
+                "Hello World",
+              ),
+              Text(
+                "Hello World",
               ),
             ],
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  Widget bloodGroupInfoWidget(context) {
+    // 148/360 41%
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.41,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: const [
+              Text(
+                "Hello World",
+              ),
+              Text(
+                "Hello World",
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
