cbfb65d404dda854621c87afe40a4f9b6d1b1d0a
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2023-02-02 18:41:17 +0530

    teams in stream

---
+++ bin/football_stream.dart
@@ -0,0 +1,31 @@
+void main() {
+  print("Hello World");
+  getDataFromStream().listen((event) {
+    print(event);
+  });
+}
+
+List<Map<String, dynamic>> footballScores = [
+  {
+    "time": 1,
+    "team": 'ENG',
+  },
+  {
+    "time": 2,
+    "team": "FRA",
+  },
+  {
+    "time": 2,
+    "team": "ENG",
+  },
+  {
+    "time": 1,
+    "team": "ENG",
+  }
+];
+
+Stream<String> getDataFromStream() async* {
+  for (var i = 0; i < footballScores.length; i++) {
+    yield footballScores[i]["team"].toString();
+  }
+}
