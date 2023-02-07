2425e9acd00acdbc3643975ff4e1f7d660c690ce
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2023-02-07 17:11:07 +0530

    mesages ui made

--- lib/screens/messages.dart
+++ lib/screens/messages.dart
@@ -3,12 +3,30 @@ import 'package:flutter/material.dart';
 class Messages extends StatelessWidget {
   const Messages({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      color: Colors.red,
-    ));
+      appBar: AppBar(
+        title: const Text('Messages'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: ["hi,", "hello", "whatspu", "send blood", "Please"]
+                    .map((e) => Container(
+                          padding: EdgeInsets.only(
+                            top: 200,
+                          ),
+                          child: Text(e),
+                        ))
+                    .toList(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
