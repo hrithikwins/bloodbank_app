47f8146fb9d691a8fe4c393bf7e527c3c8c7d97e
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2023-02-08 20:23:34 +0530

    data models

---
+++ lib/models/user_data.model.dart
@@ -0,0 +1,65 @@
+// To parse this JSON data, do
+//
+//     final userData = userDataFromJson(jsonString);
+
+import 'dart:convert';
+
+class UserData {
+  UserData({
+    this.fullName,
+    this.dateOfBirth,
+    this.age,
+    this.prevailingHealthConditions,
+    this.bloodGroup,
+  });
+
+  String? fullName;
+  String? dateOfBirth;
+  String? age;
+  PrevailingHealthConditions? prevailingHealthConditions;
+  String? bloodGroup;
+
+  UserData copyWith({
+    String? fullName,
+    String? dateOfBirth,
+    String? age,
+    PrevailingHealthConditions? prevailingHealthConditions,
+    String? bloodGroup,
+  }) =>
+      UserData(
+        fullName: fullName ?? this.fullName,
+        dateOfBirth: dateOfBirth ?? this.dateOfBirth,
+        age: age ?? this.age,
+        prevailingHealthConditions:
+            prevailingHealthConditions ?? this.prevailingHealthConditions,
+        bloodGroup: bloodGroup ?? this.bloodGroup,
+      );
+
+  factory UserData.fromRawJson(String str) =>
+      UserData.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory UserData.fromJson(Map<String, dynamic> json) => UserData(
+        fullName: json["full_name"],
+        dateOfBirth: json["date_of_birth"],
+        age: json["age"],
+        prevailingHealthConditions: json["prevailing_healthConditions"],
+        bloodGroup: json["blood_group"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "full_name": fullName,
+        "date_of_birth": dateOfBirth,
+        "age": age,
+        "prevailing_healthConditions": prevailingHealthConditions,
+        "blood_group": bloodGroup,
+      };
+}
+
+enum PrevailingHealthConditions {
+  diabetes,
+  hypertension,
+  heartDisease,
+  none,
+}
